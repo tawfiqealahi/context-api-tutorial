fd2101e5cccd67e38b74107706a77779e12d463d
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-12-08 08:51:43 -0600

    6

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React,{ createContext } from 'react';
 import './App.css';
 import CompA from './CompA';
 
-const firstName = createContext();
+const FirstName = createContext();
 
 function App() {
   return (
     <div className="App">
-      <firstName.Provider>
+      <FirstName.Provider value={"Tawfiq E "}>
       <CompA></CompA>
-      </firstName.Provider>
+      </FirstName.Provider>
     </div>
   );
 }
 
 export default App;
+export {FirstName}
--- src/CompC.js
+++ src/CompC.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { FirstName } from './App';
 
 const CompC = () => {
     return (
         <div>
+            <h1>
+                <FirstName.Consumer>
+                    {(fname)=>{
+                        return(
+                        <h1>this is first context {fname} </h1>
+                        )
+                    }}
+                </FirstName.Consumer>
+            </h1>
             compC 
         </div>
     );
 };
-
+ 
 export default CompC;
